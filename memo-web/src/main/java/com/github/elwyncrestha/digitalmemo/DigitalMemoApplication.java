be2fbdacdb54bd8cb9a7a36f5dc4a843dfe73ab1
Elvin Shrestha <elwyncrestha@gmail.com>
2020-06-11 15:44:58 +0545

    Initial Commit

---
+++ memo-web/src/main/java/com/github/elwyncrestha/digitalmemo/DigitalMemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.digitalmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalMemoApplication.class, args);
+	}
+
+}
---
+++ memo-web/src/test/java/com/github/elwyncrestha/digitalmemo/DigitalMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elwyncrestha.digitalmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
