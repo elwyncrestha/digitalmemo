bd0255596dc2fb2ed51af9ab1bff29c7f526cc0c
Elvin Shrestha <elwyncrestha@gmail.com>
2020-06-11 16:17:13 +0545

    Refactor: Removed `@SpringBootTest` annotation.

--- memo-web/src/test/java/com/github/elwyncrestha/digitalmemo/DigitalMemoApplicationTests.java
+++ memo-web/src/test/java/com/github/elwyncrestha/digitalmemo/DigitalMemoApplicationTests.java
@@ -1,9 +1,7 @@
 package com.github.elwyncrestha.digitalmemo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class DigitalMemoApplicationTests {
 
 	@Test
